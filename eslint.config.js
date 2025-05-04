d2e9a5b1cf3f0594d6285e23a69d47c75bb335a1
NDKhanh1996 <ndkhanh1996@gmail.com>
2025-05-04 14:59:45 +0700

    setup: eslint prettier

---
+++ eslint.config.js
@@ -0,0 +1,70 @@
+// https://docs.expo.dev/guides/using-eslint/
+const { defineConfig } = require('eslint/config');
+const expoConfig = require('eslint-config-expo/flat');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+
+module.exports = defineConfig([
+    expoConfig,
+    eslintPluginPrettierRecommended,
+    {
+        ignores: ['dist/*', '/.expo', 'node_modules/*'],
+        rules: {
+            'prettier/prettier': 'error',
+            'no-console': ['warn', { allow: ['warn', 'error', 'clear'] }],
+            'no-debugger': 'error',
+            'no-dupe-keys': 'error',
+            'no-duplicate-case': 'error',
+            'no-empty': 'error',
+            'no-extra-semi': 'error',
+            'no-func-assign': 'error',
+            'no-obj-calls': 'error',
+            'no-sparse-arrays': 'error',
+            'no-unreachable': 'error',
+            'no-unused-vars': ['error', { args: 'none' }],
+            'valid-typeof': 'warn',
+            curly: 'error',
+            eqeqeq: 'warn',
+            'no-else-return': 'warn',
+            'no-eval': 'error',
+            'semi-spacing': 'error',
+            'keyword-spacing': 'error',
+            'space-infix-ops': 'error',
+            'prefer-const': 'error',
+            'no-restricted-imports': [
+                'error',
+                {
+                    patterns: ['.*'],
+                },
+            ],
+            'object-curly-spacing': ['error', 'always'],
+            'padding-line-between-statements': [
+                'error',
+                {
+                    blankLine: 'always',
+                    prev: '*',
+                    next: 'return',
+                },
+                {
+                    blankLine: 'always',
+                    prev: ['const', 'let', 'var'],
+                    next: '*',
+                },
+                {
+                    blankLine: 'any',
+                    prev: ['const', 'let', 'var'],
+                    next: ['const', 'let', 'var'],
+                },
+                {
+                    blankLine: 'always',
+                    prev: 'directive',
+                    next: '*',
+                },
+                {
+                    blankLine: 'any',
+                    prev: 'directive',
+                    next: 'directive',
+                },
+            ],
+        },
+    },
+]);

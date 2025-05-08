e280e776aa5ca6ee43c25f5425ce06bd606309f3
NDKhanh1996 <ndkhanh1996@gmail.com>
2025-05-08 14:09:49 +0700

    setup: add locales

---
+++ src/utils/locales/english.ts
@@ -0,0 +1,10 @@
+export default {
+    auth: {
+        login: 'Login',
+        forgot_password: 'Forgot password?',
+        or_login_with: 'Or login with',
+        do_not_have_an_account: "Don't have an account?",
+        register_now: 'Register now',
+        logging_in: 'Logging in...',
+    },
+};
---
+++ src/utils/locales/vietnam.ts
@@ -0,0 +1,10 @@
+export default {
+    auth: {
+        login: 'Đăng nhập',
+        forgot_password: 'Quên mật khẩu?',
+        or_login_with: 'Hoặc đăng nhập bằng',
+        do_not_have_an_account: 'Bạn chưa có tài khoản?',
+        register_now: 'Đăng ký ngay',
+        logging_in: 'Đang đăng nhập...',
+    },
+};

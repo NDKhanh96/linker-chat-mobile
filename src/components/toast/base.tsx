75e52cb637ad28b3f8453a7322caeac66c98a7bd
NDKhanh1996 <ndkhanh1996@gmail.com>
2025-05-22 14:18:32 +0700

    feat: add toast

---
+++ src/components/toast/base.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+
+import { View } from '~components/themed';
+import { CloseIcon, HelpCircleIcon, Icon } from '~components/ui/icon';
+import { Toast, ToastDescription, ToastTitle, useToast } from '~components/ui/toast';
+
+export function useErrorToast() {
+    const toast = useToast();
+
+    const showErrorToast = React.useCallback(
+        (message = 'Something went wrong.') => {
+            const newId = Math.random().toString();
+
+            toast.show({
+                id: newId,
+                placement: 'top',
+                duration: 30000,
+                render: ({ id }) => (
+                    <Toast
+                        action="error"
+                        variant="outline"
+                        nativeID={'toast-' + id}
+                        className="p-4 gap-6 border-error-500 w-11/12 shadow-hard-5 flex-row justify-between self-center"
+                    >
+                        <View className="flex-row gap-3">
+                            <Icon as={HelpCircleIcon} className="stroke-error-500 mt-0.5" />
+                            <View className="flex-col gap-1">
+                                <ToastTitle size="lg" className="font-bold text-error-500">
+                                    Error!
+                                </ToastTitle>
+                                <ToastDescription size="lg">{message}</ToastDescription>
+                            </View>
+                        </View>
+                        <View className="min-[450px]:gap-3 gap-1 flex-row">
+                            <TouchableOpacity onPress={() => toast.close(id)}>
+                                <Icon as={CloseIcon} />
+                            </TouchableOpacity>
+                        </View>
+                    </Toast>
+                ),
+            });
+        },
+        [toast],
+    );
+
+    return showErrorToast;
+}
